72a27dbe51be2cce4aa67933c81589635a720674
John Nielsen <johnhayesnielsen@gmail.com>
2020-12-08 11:55:55 -0700

    attempting to get number to increment

--- assets/js/clicker.js
+++ assets/js/clicker.js
@@ -1,6 +1,6 @@
 const clicker = document.getElementById('clickBtn');
-const clicker = document.getElementById('window');
+let windowCount = document.getElementById('window');
 
 const addOne = () => {
-    clickBtn.onClick
-}
+    windowCount++;
+};
