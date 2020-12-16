0b8b8efcb401aee57a762bbdae0803bb54b5d071
John Nielsen <johnhayesnielsen@gmail.com>
2020-12-16 09:49:33 -0700

    added auto-increase

--- assets/js/clicker.js
+++ assets/js/clicker.js
@@ -4,3 +4,18 @@ let windowCount = document.getElementById('window');
 const addOne = () => {
     windowCount.value++;
 };
+
+setTimeout(start, 5000);
+
+var i = 1;
+
+function start() {
+  setInterval(increase, 10000);
+}
+
+function increase() {
+    if (i < 100) {
+      i++;
+      windowCount.value = i;
+    }
+};
